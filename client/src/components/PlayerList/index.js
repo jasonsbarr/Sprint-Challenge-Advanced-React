5bd11a7dfd5cc3bbaeabeee794c57d612260f653
Jason Barr <jason@jasonsbarr.com>
2019-11-09 14:34:40 -0600

    Refactor PlayerList to use useFilter

--- client/src/components/PlayerList/index.js
+++ client/src/components/PlayerList/index.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./Player";
+import useFilter from "../../hooks/useFilter";
 
 const PlayerList = ({ players }) => {
-  return players.length ? (
-    <ul data-testid="player-list">
-      {players.map(player => (
-        <Player key={player.name} player={player} />
-      ))}
-    </ul>
+  const [searchString, setSearchString] = useState("");
+
+  const searchFn = player =>
+    player.name.toLowerCase().includes(searchString.toLowerCase());
+
+  const [playerData] = useFilter(searchFn, players, searchString);
+
+  return playerData.length ? (
+    <>
+      <input
+        type="search"
+        placeholder="Search for players by name..."
+        value={searchString}
+        onChange={e => setSearchString(e.target.value)}
+      />
+      <ul data-testid="player-list">
+        {playerData.map(player => (
+          <Player key={player.name} player={player} />
+        ))}
+      </ul>
+    </>
   ) : (
     <div>Players not loaded...</div>
   );
