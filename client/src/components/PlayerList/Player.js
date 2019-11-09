84f9759c7a51303941bcafb4621825242790c725
Jason Barr <jason@jasonsbarr.com>
2019-11-09 13:42:15 -0600

    Destructure props correctly

--- client/src/components/PlayerList/Player.js
+++ client/src/components/PlayerList/Player.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Player = ({ name, country, searches }) => (
-  <li>
-    <span>Player:</span> {name} - <span>Team:</span> {country} -{" "}
-    <span>Searches:</span> {searches}
-  </li>
-);
+const Player = ({ player: { name, country, searches } }) => {
+  return (
+    <li>
+      <span>Player:</span> {name} – <span>Team:</span> {country} –{" "}
+      <span>Searches:</span> {searches}
+    </li>
+  );
+};
 
 export default Player;
