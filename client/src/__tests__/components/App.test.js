f2cbb48488d7be13b1b0862ed6e9c29834eea4d3
Jason Barr <jason@jasonsbarr.com>
2019-11-09 13:39:43 -0600

    Stub out data for App & add snapshot test

--- client/src/__tests__/components/App.test.js
+++ client/src/__tests__/components/App.test.js
@@ -1,9 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, cleanup, getByTestId } from "@testing-library/react";
 import App from "../../components/App";
+import playerData from "../../../__mocks__/players";
+import { act } from "react-dom/test-utils";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let container;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(cleanup);
+
+describe("App component tests", () => {
+  it("renders without crashing", () => {
+    render(<App />, container);
+  });
+
+  it("Shows a list of users", async () => {
+    jest.spyOn(window, "fetch").mockImplementation(() => {
+      const response = {
+        json: () => Promise.resolve(playerData),
+      };
+
+      return Promise.resolve(response);
+    });
+
+    await act(async () => {
+      const { getByTestId } = render(<App />, container);
+    });
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,52 @@
 import React, { Component } from "react";
 import PlayerList from "./PlayerList";
 
+const playerData = [
+  { name: "Alex Morgan", country: "United States", searches: 100 },
+  { name: "Megan Rapinoe", country: "United States", searches: 99 },
+  { name: "Marta", country: "Brazil", searches: 18 },
+  { name: "Rose Lavelle", country: "United States", searches: 11 },
+  { name: "Carli Lloyd", country: "United States", searches: 9 },
+  { name: "Julie Ertz", country: "United States", searches: 8 },
+  { name: "Christen Press", country: "United States", searches: 8 },
+  { name: "Kelley O'Hara", country: "United States", searches: 8 },
+  { name: "Tobin Heath", country: "United States", searches: 7 },
+  { name: "Wendie Renard", country: "France", searches: 7 },
+  { name: "Kosovare Asllani", country: "Sweden", searches: 6 },
+  { name: "Lieke Martens", country: "Netherlands", searches: 6 },
+  { name: "Amandine Henry", country: "France", searches: 5 },
+  { name: "Ali Krieger", country: "United States", searches: 5 },
+  { name: "Mallory Pugh", country: "United States", searches: 4 },
+  { name: "Samantha Kerr", country: "Australia", searches: 4 },
+  { name: "Christiane Endler", country: "Chile", searches: 4 },
+  {
+    name: "Daniëlle van de Donk",
+    country: "Netherlands",
+    searches: 4,
+  },
+  { name: "Lucy Bronze", country: "England", searches: 4 },
+  { name: "Ashlyn Harris", country: "United States", searches: 4 },
+  { name: "Eugénie Le Sommer", country: "France", searches: 4 },
+  { name: "Jackie Groenen", country: "Netherlands", searches: 4 },
+  { name: "Abby Dahlkemper", country: "United States", searches: 3 },
+  { name: "Steph Houghton", country: "England", searches: 3 },
+  { name: "Vivianne Miedema", country: "Netherlands", searches: 3 },
+];
+
 class App extends Component {
   state = { players: [] };
 
+  componentDidMount() {
+    this.setState({ players: playerData });
+  }
+
   render() {
     const { players } = this.state;
+
     return players.length ? (
-      <PlayerList players={players} />
+      <div data-testid="app">
+        <PlayerList players={players} />
+      </div>
     ) : (
       <div>Haven't found any players yet...</div>
     );
