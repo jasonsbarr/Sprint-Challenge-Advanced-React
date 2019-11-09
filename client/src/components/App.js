87df4f228483efdb3278225107cd2b1613937814
Jason Barr <jason@jasonsbarr.com>
2019-11-09 12:43:27 -0600

    Move App to components dir

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = { players: [] };
+
+  render() {
+    return <div>Players!</div>;
+  }
+}
+
+export default App;
